3868e1b284b943c20525930b4d238bfe9d44f479
RUPESH KUMAR <98460861+Rupeshkumar4@users.noreply.github.com>
2023-02-11 09:05:51 +0530

    Add files via upload
    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import turtle
+i = int(input("Enter the vale of i=> "));
+t = turtle.Turtle()
+t.screen.bgcolor("black")
+t.pensize(3)
+t.color("brown")
+t.left(90)
+t.backward(100)
+t.speed(650)
+t.shape("arrow")
+
+
+def tree(i):
+    if i<10:
+        return
+    else:
+        t.forward(i)
+        t.color("green")
+        t.circle(3)
+        t.color("#9c4a1a")
+        t.left(30)
+        tree(3*i/4)
+        t.right(60)
+        tree(3*i/4)
+        t.left(30)
+        t.backward(i)
+tree(i)
+turtle()
